4f9b6a8002dabcc5c5bdeb570befd7df1e8399e0
UKiranLearnsToCookMeth <123247301+UKiranLearnsToCookMeth@users.noreply.github.com>
2023-05-29 17:01:44 -0500

    First half of the blackjack code (just code not design) done, will do dealer side tomorrow.

--- Granbling/Form1.Designer.cs
+++ Granbling/Form1.Designer.cs
@@ -33,7 +33,14 @@ namespace Granbling
             this.btnShowVar = new System.Windows.Forms.Button();
             this.btnSlots = new System.Windows.Forms.Button();
             this.btnBlackjack = new System.Windows.Forms.Button();
+            this.pnlBlackjack = new System.Windows.Forms.Panel();
+            this.btnBJ_Hit = new System.Windows.Forms.Button();
+            this.lblBJ_DealerCards = new System.Windows.Forms.Label();
+            this.lblBJ_PlayerCards = new System.Windows.Forms.Label();
+            this.btnBJ_Stay = new System.Windows.Forms.Button();
+            this.btnBJ_Exit = new System.Windows.Forms.Button();
             this.gboxDebug.SuspendLayout();
+            this.pnlBlackjack.SuspendLayout();
             this.SuspendLayout();
             // 
             // gboxDebug
@@ -77,16 +84,79 @@ namespace Granbling
             this.btnBlackjack.UseVisualStyleBackColor = true;
             this.btnBlackjack.Click += new System.EventHandler(this.btnBlackjack_Click);
             // 
+            // pnlBlackjack
+            // 
+            this.pnlBlackjack.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pnlBlackjack.Controls.Add(this.btnBJ_Exit);
+            this.pnlBlackjack.Controls.Add(this.btnBJ_Stay);
+            this.pnlBlackjack.Controls.Add(this.btnBJ_Hit);
+            this.pnlBlackjack.Controls.Add(this.lblBJ_DealerCards);
+            this.pnlBlackjack.Controls.Add(this.lblBJ_PlayerCards);
+            this.pnlBlackjack.Location = new System.Drawing.Point(12, 12);
+            this.pnlBlackjack.Name = "pnlBlackjack";
+            this.pnlBlackjack.Size = new System.Drawing.Size(851, 383);
+            this.pnlBlackjack.TabIndex = 1;
+            // 
+            // btnBJ_Hit
+            // 
+            this.btnBJ_Hit.Location = new System.Drawing.Point(203, 215);
+            this.btnBJ_Hit.Name = "btnBJ_Hit";
+            this.btnBJ_Hit.Size = new System.Drawing.Size(75, 23);
+            this.btnBJ_Hit.TabIndex = 2;
+            this.btnBJ_Hit.Text = "Hit!";
+            this.btnBJ_Hit.UseVisualStyleBackColor = true;
+            this.btnBJ_Hit.Click += new System.EventHandler(this.btnBJ_Hit_Click);
+            // 
+            // lblBJ_DealerCards
+            // 
+            this.lblBJ_DealerCards.AutoSize = true;
+            this.lblBJ_DealerCards.Location = new System.Drawing.Point(202, 180);
+            this.lblBJ_DealerCards.Name = "lblBJ_DealerCards";
+            this.lblBJ_DealerCards.Size = new System.Drawing.Size(76, 13);
+            this.lblBJ_DealerCards.TabIndex = 1;
+            this.lblBJ_DealerCards.Text = "Dealers Cards:";
+            // 
+            // lblBJ_PlayerCards
+            // 
+            this.lblBJ_PlayerCards.AutoSize = true;
+            this.lblBJ_PlayerCards.Location = new System.Drawing.Point(200, 154);
+            this.lblBJ_PlayerCards.Name = "lblBJ_PlayerCards";
+            this.lblBJ_PlayerCards.Size = new System.Drawing.Size(62, 13);
+            this.lblBJ_PlayerCards.TabIndex = 0;
+            this.lblBJ_PlayerCards.Text = "Your Cards:";
+            // 
+            // btnBJ_Stay
+            // 
+            this.btnBJ_Stay.Location = new System.Drawing.Point(284, 215);
+            this.btnBJ_Stay.Name = "btnBJ_Stay";
+            this.btnBJ_Stay.Size = new System.Drawing.Size(75, 23);
+            this.btnBJ_Stay.TabIndex = 3;
+            this.btnBJ_Stay.Text = "Stay!";
+            this.btnBJ_Stay.UseVisualStyleBackColor = true;
+            this.btnBJ_Stay.Click += new System.EventHandler(this.btnBJ_Stay_Click);
+            // 
+            // btnBJ_Exit
+            // 
+            this.btnBJ_Exit.Location = new System.Drawing.Point(202, 284);
+            this.btnBJ_Exit.Name = "btnBJ_Exit";
+            this.btnBJ_Exit.Size = new System.Drawing.Size(75, 23);
+            this.btnBJ_Exit.TabIndex = 4;
+            this.btnBJ_Exit.Text = "Exit";
+            this.btnBJ_Exit.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1246, 450);
+            this.Controls.Add(this.pnlBlackjack);
             this.Controls.Add(this.gboxDebug);
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "Granbling - Jurm 2 in 2024";
             this.gboxDebug.ResumeLayout(false);
+            this.pnlBlackjack.ResumeLayout(false);
+            this.pnlBlackjack.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -97,6 +167,12 @@ namespace Granbling
         private System.Windows.Forms.Button btnShowVar;
         private System.Windows.Forms.Button btnSlots;
         private System.Windows.Forms.Button btnBlackjack;
+        private System.Windows.Forms.Panel pnlBlackjack;
+        private System.Windows.Forms.Label lblBJ_PlayerCards;
+        private System.Windows.Forms.Label lblBJ_DealerCards;
+        private System.Windows.Forms.Button btnBJ_Hit;
+        private System.Windows.Forms.Button btnBJ_Exit;
+        private System.Windows.Forms.Button btnBJ_Stay;
     }
 }
 
--- Granbling/Form1.cs
+++ Granbling/Form1.cs
@@ -12,7 +12,22 @@ namespace Granbling
 {
     public partial class Form1 : Form
     {
-        int[] granCoords = {0, 0}; // we BALL.
+        // grandma basics/shared values
+        int[] granCoords = {0, 0}; // we BALL. [Todo: The entire movement code] [sidenote, got alot of help with learning these from https://www.w3schools.com/cs/cs_arrays.php]
+        int granCash = 1000; //  starting sum of cash given to Grandma. Deplets over time, and through the usage of slots/blackjack. [Todo: gain access to the trello board so i can see the right amount]
+        int energy = 50; // unused right now, will be used for the amount of actions Grandma can do each day. [TODO: movement code]
+        bool canGrandmaMove = false; // unused, will be for grandma movement
+
+        // slot variables 
+
+        // blackjack variables, all of these have a default value just for sake of squashing potential bugs before they show up (foreshadowing)
+        // also, some of the const ints aren't there (bj is heavily based off of the demo code) because its just easier for me to type out "9" or "21" then "blablabla" and i don't really see a point asides from "look at me!" energy.
+        int[] bjPlayerCards = { 0, 0, 0, 0, 0 };
+        int[] bjDealerCards = { 0, 0, 0 };
+        int bjPlayerTotal = 0;
+        int bjPlayerDrawCount = 0;
+        bool bjGameInProgress = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -20,10 +35,97 @@ namespace Granbling
 
         private void btnSlots_Click(object sender, EventArgs e) // Most of this code is just taken from the slot machine test...thing....smth? either way, it's used here.
         {
-
+            canGrandmaMove = false;
         }
 
         private void btnBlackjack_Click(object sender, EventArgs e) // heavily uses code/layout of the demo! Will be fixed/improved!
+        {
+            pnlBlackjack.Visible = true;
+            canGrandmaMove = false;
+        }
+
+        private void btnBJ_Hit_Click(object sender, EventArgs e)
+        {
+
+            //gives a random card between 1 and 9.
+            Random rand = new Random();
+            if (bjPlayerCards[0] == 0)
+            { //draws 2 random cards, displays them, and sets the draw count to 0.
+                bjGameInProgress = true;
+                bjPlayerCards[0] = rand.Next(1, 9);
+                bjPlayerCards[1] = rand.Next(1, 9);
+                lblBJ_PlayerCards.Text = "Your cards:" + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ".";
+                bjPlayerDrawCount = 0;
+                bjPlayerTotal = bjPlayerCards[0] + bjPlayerCards[1];
+            }
+            else if (bjPlayerCards[2] == 0 && bjPlayerTotal <= 20) // i dont even know whats goin on i can NOT help you. all you need to know is this is just blackjack going on.
+            {
+                bjPlayerCards[2] = rand.Next(1, 9);
+                bjPlayerDrawCount = 1; // theres no reason why it'd need to be += because in any circumstance it should be 1.
+                bjPlayerTotal = bjPlayerCards[0] + bjPlayerCards[1] + bjPlayerCards[2];
+                if (bjPlayerTotal == 21)
+                {
+                    lblBJ_PlayerCards.Text = "You won! Your cards were: " + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", and " + bjPlayerCards[2] + ". totalling up to 21!";
+                    bjGameInProgress = false;
+                }
+                else if (bjPlayerTotal > 21)
+                {
+                    lblBJ_PlayerCards.Text = "Bust! Your cards were: " + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", and " + bjPlayerCards[2] + ", Good luck next time!";
+                    bjGameInProgress = false;
+                }
+                else
+                {
+                    lblBJ_PlayerCards.Text = "Your cards:" + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", " + bjPlayerCards[2] + ".";
+                }
+
+            }
+            else if (bjPlayerCards[3] == 0 && bjPlayerTotal <= 20)
+            { // copied the previous lol...
+                bjPlayerCards[3] = rand.Next(1, 9);
+                bjPlayerDrawCount = 2;
+                bjPlayerTotal = bjPlayerCards[0] + bjPlayerCards[1] + bjPlayerCards[2] + bjPlayerCards[3];
+                if (bjPlayerTotal == 21)
+                {
+                    lblBJ_PlayerCards.Text = "You won! Your cards were: " + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", " + bjPlayerCards[2] + ", and " + bjPlayerCards[3] + ". totalling up to 21!";
+                    bjGameInProgress = false;
+                }
+                else if (bjPlayerTotal > 21)
+                {
+                    lblBJ_PlayerCards.Text = "Bust! Your cards were: " + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", " + bjPlayerCards[2] + ", and " + bjPlayerCards[3] + ", Good luck next time!";
+                    bjGameInProgress = false;
+                }
+                else
+                {
+                    lblBJ_PlayerCards.Text = "Your cards:" + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", " + bjPlayerCards[2] + ", " + bjPlayerCards[3] + ".";
+                }
+
+            }
+            else if (bjPlayerCards[4] == 0 && bjPlayerTotal <= 20)
+            {
+                bjPlayerCards[4] = rand.Next(1, 9);
+                bjPlayerDrawCount = 3;
+                bjPlayerTotal = bjPlayerCards[0] + bjPlayerCards[1] + bjPlayerCards[2] + bjPlayerCards[3] + bjPlayerCards[4];
+                if (bjPlayerTotal == 21)
+                {
+                    lblBJ_PlayerCards.Text = "You won! Your cards were: " + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", " + bjPlayerCards[2] + ", " + bjPlayerCards[3] + ", and " + bjPlayerCards[4] + ", totalling up to 21!";
+                }
+                else if (bjPlayerTotal > 21)
+                {
+                    lblBJ_PlayerCards.Text = "Bust! Your cards were: " + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", " + bjPlayerCards[2] + ", " + bjPlayerCards[3] + ", and " + bjPlayerCards[4] + ", Good luck next time!";
+                }
+                else
+                {
+                    lblBJ_PlayerCards.Text = "Your cards:" + bjPlayerCards[0] + ", " + bjPlayerCards[1] + ", " + bjPlayerCards[2] + ", " + bjPlayerCards[3] + ", " + bjPlayerCards[4] + ".";
+                }
+                btnBJ_Hit.Enabled = false;
+            }
+            else
+            {
+                btnBJ_Hit.Enabled = false;
+            }
+        }
+
+        private void btnBJ_Stay_Click(object sender, EventArgs e)
         {
 
         }
